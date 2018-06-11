ad4fe2840ca4c284628fa6cd17aa3d1e562bfc85
huangqizai <17710946884@163.com>
2018-06-11 16:19:25 +0800

    Initial Commit

---
+++ kinema-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kinema-swift
+//
+//  Created by huangqizai on 2018/6/11.
+//  Copyright © 2018年 FYCK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
